48f7f495f2d2963a5474543d0d302c1a3c469fca
MoeezAhmed1609 <moeezahmed.saeed@gmail.com>
2023-01-20 12:42:49 +0500

    first commit

--- next.config.js
+++ next.config.js
@@ -9,5 +9,22 @@ const nextConfig = {
     return config
   }
 }
+module.exports = {
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
+ };
 
 module.exports = nextConfig
