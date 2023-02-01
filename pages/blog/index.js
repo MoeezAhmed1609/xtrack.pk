349b14b5e1b2f555b1791e36933a140a56221a54
MoeezAhmed1609 <moeezahmed.saeed@gmail.com>
2023-02-01 13:34:00 +0500

    first commit

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -11,57 +11,60 @@ const blog = () => {
       .fetch(`*[_type == 'blogs']`)
       .then((data) => {
         setPosts(data)
-        // console.log(posts)
       })
       .catch(console.error)
-  })
+  }, [])
   return (
     <>
-    <Head>
-      <title>Blogs | Xtrack.pk</title>
-    </Head>
+      <Head>
+        <title>Blogs | Xtrack.pk</title>
+      </Head>
       <div className="container-fluid">
         <div className="container text-center py-4 border-bottom border-danger border-2">
           <span className="fs-4 text-uppercase fw-bold">Blogs by XTRACK</span>
         </div>
-        {posts ? (
-          posts.map((post) => (
-            <Link
-              href={`/blog/${post.Slug.current}`}
-              className="text-decoration-none text-dark"
-              key={post._id}
-            >
-              <div className="row py-4">
-                <div className="col-lg-7 col-12">
-                  <img
-                    className="blog-img"
-                    alt={post.title}
-                    src={urlFor(post.coverimage?.asset._ref)}
-                  />
-                </div>
-                <div className="col-lg-5 col-12">
-                  <div className="blog-content pt-2">
-                    <h4 className="fw-bold">{post.title}</h4>
-                    <BlockContent
-                      projectId="ke3vv5hk"
-                      dataset={'production'}
-                      blocks={post.content}
-                      className="blog-text text-secondary"
-                    />
-                    <button
-                      type="button"
-                      className="btn btn-outline-danger rounded-0 mt-3 ms-2 text-uppercase"
-                      style={{ width: '45%' }}
-                    >
-                      Read More
-                    </button>
+        {posts?.length > 0 ? (
+          posts?.map((post) => (
+            <div>
+              <Link
+                href={`/blog/${post?.Slug?.current}`}
+                className="text-decoration-none text-dark"
+                key={post?._id}
+              >
+                <div className="row py-4">
+                  {post?.coverimage ? (
+                    <div className="col-lg-7 col-12">
+                      <img
+                        className="blog-img"
+                        alt={post?.title}
+                        src={urlFor(post?.coverimage?.asset?._ref)}
+                      />
+                    </div>
+                  ) : null}
+                  <div className="col-lg-5 col-12">
+                    <div className="blog-content pt-2">
+                      <h4 className="fw-bold">{post?.title}</h4>
+                      <BlockContent
+                        projectId="ke3vv5hk"
+                        dataset={'production'}
+                        blocks={post?.content}
+                        className="blog-text text-secondary"
+                      />
+                      <button
+                        type="button"
+                        className="btn btn-outline-danger rounded-0 mt-3 ms-2 text-uppercase"
+                        style={{ width: '45%' }}
+                      >
+                        Read More
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           ))
         ) : (
-          <div className="container text-center">
+          <div className="container text-center my-5">
             <span className="text-uppercase fs-5">No posts to show!</span>
           </div>
         )}
